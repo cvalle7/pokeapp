de417ae22a4d753e3ab8b89a09b09a2c3f0190a2
cvalle7 <cvb16@gmail.com>
2024-11-27 09:56:44 +0100

    [WIP] - reusable cards

--- src/components/Pokemon.tsx
+++ src/components/Pokemon.tsx
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import { usePokemon } from "../contexts/pokemon.context";
 import { useNavigate } from "react-router-dom";
-import Pokecard from "./Poke-card";
+import Pokecard from "./reusable/Poke-card";
 
 
 function Pokemon() {
