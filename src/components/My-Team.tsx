56fe2d5740a9922d15539957dec2a57154b2bf44
cvalle7 <cvb16@gmail.com>
2024-11-29 11:08:32 +0100

    [WIP] - My team finished

--- src/components/My-Team.tsx
+++ src/components/My-Team.tsx
@@ -1,12 +1,41 @@
-import { useEffect } from "react";
 import { useMyTeam } from "../contexts/my-team.context";
 import { usePokedex } from "../contexts/pokedex.context";
-import Pokecard from "./reusable/Poke-card";
 
 
 function MyTeam() {
     const { myTeam, setMyTeam } = useMyTeam()
     const { pokedex } = usePokedex()
+
+    const renderTeam = (p: any) => {
+        console.log(p);
+        return (
+            <div className="team-card-container">
+                {p ? (
+                    <>
+                        <div key={p.name} className="team-card">
+                            <div className="avatar">
+                                <img key={p.name} alt={p.name} src={p.data.sprites.versions["generation-iii"].emerald.front_default} onClick={() => handlePokemonSelected(p)} />
+                            </div>
+                            <div className="team-info">
+                                <p>Lv. 100</p>
+                                {p.name}
+                                <p className="stamine"></p>
+                                {p.data.base_experience}/{p.data.base_experience}
+                            </div>
+                        </div>
+                        { }
+                        <button className="send-pc" onClick={() => handleSendPc(p)}>Send to PC</button>
+                    </>
+
+                ) : (
+                    <>
+                        <div className="team-card disabled">
+                        </div>
+                    </>
+                )}
+            </div>
+        )
+    }
 
     const handlePokemonSelected = (pokemon: any) => {
         if (myTeam && myTeam.pokemons.length > 0) {
@@ -20,6 +49,14 @@ function MyTeam() {
         }
     }
 
+    const handleLoop = () => {
+        const elements = [];
+        for (let index = 0; index < 6; index++) {
+            elements.push(renderTeam(myTeam?.pokemons[index]))
+        }
+        return elements;
+    }
+
     const handleSendPc = (pokemon: any) => {
         const filter = myTeam?.pokemons.filter(p => p.name !== pokemon.name);
         if (myTeam && myTeam.pokemons.length == 1) {
@@ -29,7 +66,6 @@ function MyTeam() {
                 setMyTeam({ pokemons: [...filter] })
             }
         }
-
     }
 
     return (
@@ -51,14 +87,7 @@ function MyTeam() {
                         })}
                     </div>
                     <div className="team-container">
-                        {myTeam?.pokemons.map(p => {
-                            return (
-                                <div className="team-card">
-                                    <Pokecard pokemon={p} />
-                                    <button onClick={() => handleSendPc(p)} className="send-pc">Send to PC</button>
-                                </div>
-                            )
-                        })}
+                        {handleLoop()}
                     </div>
                 </>
 
